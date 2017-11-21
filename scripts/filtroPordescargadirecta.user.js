b20418c4138efcb0f7a181d08a1f72cb35862277
Deagle <xxjose3xx@users.noreply.github.com>
2017-11-21 12:49:56 +0100

    Update filtroPordescargadirecta.user.js

--- scripts/filtroPordescargadirecta.user.js
+++ scripts/filtroPordescargadirecta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name        tmp
-// @namespace   tmp.es
-// @description temporal
+// @name        Filtro 2010
+// @namespace   pordescargadirecta
+// @description Añade un filtro para películas a partir del 2010.
 // @include     *pordescargadirecta.com/forumdisplay.php?*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @version     1
@@ -13,13 +13,3 @@ var lista = document.getElementsByClassName("title");
 for(var i=lista.length-1; i>=0; i--)
   if(lista[i].innerHTML.search("201") == -1)
     lista[i].parentNode.parentNode.parentNode.parentNode.remove();
-
-window.onkeyup = function(e) {
-   var key = e.keyCode ? e.keyCode : e.which;
-
-   if (key == 37) {
-       document.getElementsByClassName("prev_next")[0].children[0].click(); // Anterior
-   } else if (key == 39) {
-       document.getElementsByClassName("prev_next")[1].children[0].click(); // Siguiente
-   }
-}

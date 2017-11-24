4031f96c220f35a51485c9ca613b7bdba70b60d9
Deagle <xxjose3xx@users.noreply.github.com>
2017-11-24 16:27:45 +0100

    Update farmAssist.user.js

--- scripts/farmAssist.user.js
+++ scripts/farmAssist.user.js
@@ -4,28 +4,18 @@
 // @description Automatiza el envío de la página del asistente de granja.
 // @include     https://*.guerrastribales.es/game.php?village*screen=am_farm*
 // @icon        https://dses.innogamescdn.com/8.105/35420/graphic/icons/farm_assistent.png
-// @version     2
+// @version     2.1
 // @grant       none
 // ==/UserScript==
 
-function iniciarCadenaDeAtaques() {
+function initAttackChain() {
 
-    var lista = $('[class^="report_"]');
-    var plantilla = $('a[class^="farm_village"]:first').attr('onclick').match(/\d+/g)[1];
-    var indice = 0;
-    var longitud = lista.length;
-    var e = TribalWars.buildURL("POST", Accountmanager.send_units_link, null);
-    var t = {
-        target: 0,
-        template_id: plantilla,
-        source: game_data.village.id
-    };
+    var target_list = $('[class^="report_"]');
+    var template = $('a[class^="farm_village"]:first').attr('onclick').match(/\d+/g)[1];
+    var longitud = target_list.length;
+    var indice = 0;   
 
-    function enviarAtaque(objetivo, plantilla) {
-
-        console.log("Enviando ataque... (" + (indice+1) + "/" + longitud + ")");
-
-        t.target = objetivo;
+    function sendAttack(objetivo) {
 
         var r = function(e) {
             $(".farm_village_" + objetivo).addClass("farm_icon_disabled").addClass("done");
@@ -39,23 +29,28 @@ function iniciarCadenaDeAtaques() {
             Accountmanager.farm.no_remove.hasOwnProperty(objetivo) && delete Accountmanager.farm.no_remove[objetivo];
         };
 
-        if(!Accountmanager.farm.unitsAppearAvailableAB(plantilla))
+        if(!Accountmanager.farm.unitsAppearAvailableAB(template))
             UI.ErrorMessage(_("8c51142762038a5b8e6c677aabcbedff"));
         else
             $.ajax({
-                url: e + "&client_time=" + Math.round(Timing.getCurrentServerTime() / 1e3),
-                data: t,
+                url: TribalWars.buildURL("POST", Accountmanager.send_units_link, null) + "&client_time=" + Math.round(Timing.getCurrentServerTime() / 1e3),
+                data: {
+                    target: objetivo,
+                    template_id: template,
+                    source: game_data.village.id
+                },
                 type: "POST",
                 dataType: "json",
                 headers: {"TribalWars-Ajax": 1},
                 success: function(a) {
                     TribalWars.handleResponse(a, r, i);
-                    console.log("Ataque enviado con éxito! (" + (indice+1) + "/" + longitud + ")");
+                    Accountmanager.farm.updateNonAttacked(objetivo);
                     if(++indice < longitud)
-                        enviarAtaque(lista[indice].id.slice(8), plantilla);
+                        sendAttack(target_list[indice].id.slice(8));
+                    else
+                        TribalWars.hideLoadingIndicator();
                 },
                 error: function(a, e) {
-                    console.log("Se ha producido un error. (" + (indice+1) + "/" + longitud + ")");
                     if(0 !== a.readyState) {
                         if(429 === a.status)
                             UI.ErrorMessage(_("f1ac6646f49d65cbe50901b805abfbf8"), 3e3);
@@ -65,15 +60,14 @@ function iniciarCadenaDeAtaques() {
                 }
             });  
            
-        Accountmanager.farm.updateNonAttacked(objetivo);
+        //Accountmanager.farm.updateNonAttacked(objetivo);
     }
 
     TribalWars.showLoadingIndicator();
-    enviarAtaque(lista[indice].id.slice(8), plantilla);
-    TribalWars.hideLoadingIndicator();
+    sendAttack(target_list[indice].id.slice(8));   
 }
 
 if(document.URL.indexOf("screen=am_farm") != -1)
-    console.clear(), console.log("Iniciando cadena de ataques..."), iniciarCadenaDeAtaques();
+    initAttackChain();
 else
-  alert("Este script se ejecuta desde el asistente de granaja!");
+    alert("Este script se ejecuta desde el asistente de granaja!");

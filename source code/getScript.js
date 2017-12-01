b7de2ca5554849d1c9a0811cdf7f8a207a2b6036
Deagle <xxjose3xx@users.noreply.github.com>
2017-12-01 23:12:21 +0100

    Update getScript.js

--- source code/getScript.js
+++ source code/getScript.js
@@ -1,5 +1,5 @@
 $.ajax({
-    url: "",
+    url: "https://raw.githubusercontent.com/xxjose3xx/javascript/master/scripts/farmAssist.user.js",
     dataType: "text",
     success: function(data, status) {
         eval(data);
